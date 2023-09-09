07e4a813e2080ea94c15597828e34a63fb1c1424
taco.chen <taco.chen@besi.com>
2023-09-10 00:51:33 +0800

    Get last workout from csv for default reference\n Refactor

--- trainRecord.py
+++ trainRecord.py
@@ -6,41 +6,68 @@ csvFileName = 'workouts.csv'
 
 timeFormatString = "%Y-%m-%d %H:%M:%S"
 
+# get workout names and categories from options.json
+workoutNames = []
+categories = {}
 with open('options.json') as f:
     data = json.load(f)
     f.close()
+    for workoutRec in data['workouts']:
+        workoutNames.append(workoutRec['Name'])
+        for category in workoutRec['categories']:
+            if category not in categories.keys():
+                categories[category] = []
+            categories[category].append(workoutRec['Name'])
 
-workoutNames = []
-categories = {}
-for workoutRec in data['workouts']:
-    workoutNames.append(workoutRec['Name'])
-    for category in workoutRec['categories']:
-        if category not in categories.keys():
-            categories[category] = []
-        categories[category].append(workoutRec['Name'])
-
-print(categories)
-
+# init last recorders
 lastWorkoutId = 0
 lastWeight = '0'
 lastRep = '20'
 lastTimeStamp = datetime.datetime.now()
 
+# get last workout for default select
+with open(csvFileName, 'r') as csvFile:
+    lstRec = list(csv.reader(csvFile))
+    if len(lstRec) > 0:
+        lastRec = lstRec[-1]
+        if lastRec[1] in workoutNames:
+            lastWorkoutId = workoutNames.index(lastRec[1])
+
 print("work out start at:" + lastTimeStamp.strftime(timeFormatString))
 
 while True:
     # print workout options        
-    id = 0
-    for workoutName in workoutNames:
-        print(str(id) + ": " + workoutName)
-        id = id + 1
+    for id in range(len(workoutNames)):
+        print(str(id) + ": " + workoutNames[id])
 
-    inVal = input("which work out? (" + str(lastWorkoutId) + ": " + workoutNames[lastWorkoutId] + ') ')
+    inVal = input("which work out? q:exit, c: select by category, or enter the workout name to search or add (" + str(lastWorkoutId) + ": " + workoutNames[lastWorkoutId] + ') ')
+    # use last workout
     if inVal == '':
         workoutId = lastWorkoutId
         workout = workoutNames[workoutId]
+    # quit
     elif inVal == 'q':
         break
+    # select by category 
+    elif inVal == 'c':
+        categoriesKeys = list(categories.keys())
+        for keyId in range(len(categoriesKeys)):
+            print(str(keyId) + ": " + categoriesKeys[keyId])
+
+        while True:
+            inVal = int(input('which category?'))
+            if (inVal >= len(categoriesKeys) or inVal < 0):
+                continue
+            selectedCat = categories[categoriesKeys[inVal]]
+            for id in range(len(selectedCat)):
+                print(str(id) + ": " + selectedCat[id])
+            inVal = int(input('which workout?'))
+            if (inVal >= len(selectedCat) or inVal < 0):
+                continue
+            workout = selectedCat[inVal]
+            workoutId = workoutNames.index(workout)
+            break
+    # select from list
     elif inVal.isnumeric():
         workoutId = int(inVal)
         workout = workoutNames[workoutId]
@@ -73,10 +100,12 @@ while True:
                 lastWeight = row[2]
                 break
 
+    # get weight and rep
     while True:
         weight = rep = ''
 
         inputs = input("weight(Kg)?(" + lastWeight + '), q to select new workout. ')
+        # use last weight
         if inputs == '':
             inputs = lastWeight
         if inputs == 'q':
@@ -112,6 +141,7 @@ while True:
         lastWorkoutId = workoutId
         lastTimeStamp = timeStamp
 
+        # append record to workout.csv
         with open(csvFileName, 'a') as csvFile:
             csvWriter = csv.writer(csvFile)
             csvWriter.writerow(List)
