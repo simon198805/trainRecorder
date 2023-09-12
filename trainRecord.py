8b8f70a9ac758b148e012a9b474a4efcf0c23dbe
taco.chen <taco.chen@besi.com>
2023-09-12 12:13:32 +0800

    print duration from last workout in cat list

--- trainRecord.py
+++ trainRecord.py
@@ -2,13 +2,21 @@ import datetime
 import json
 import csv
 
+class UserCancelException(Exception):
+    "User cancel"
+    pass
+
 csvFileName = 'workouts.csv'
 workoutOptionFileName = 'options.json'
 timeFormatString = "%Y-%m-%d %H:%M:%S"
 
 workoutNames = []
 categories = {}
-optionJsonData = {'workouts':[]}
+optionJsonData = {'workouts': []}
+workoutSelector = 'category'
+
+def getDatetimeFromStr(strDatetime:str) -> datetime.datetime:
+    return datetime.datetime.strptime(strDatetime, timeFormatString)
 
 def readWorkoutOptions():
     with open(workoutOptionFileName) as f:
@@ -26,6 +34,18 @@ def readWorkoutOptions():
                     categories[category] = []
                 categories[category].append(workoutRec['Name'])
 
+
+def getClosestWorkoutDateInCsv(workout) -> datetime.datetime:
+    with open(csvFileName, 'r') as csvFile:
+        lstRec = list(csv.reader(csvFile))
+        csvFile.close()
+        for rec in reversed(lstRec):
+            if len(rec) > 1:
+                if rec[1] == workout:
+                    return getDatetimeFromStr(rec[0])
+        raise ValueError('workout "' + workout + '" not found')
+
+
 def getLastWorkoutIdFromCsv()->int:
     with open(csvFileName, 'r') as csvFile:
         lstRec = list(csv.reader(csvFile))
@@ -42,12 +62,59 @@ def printListWithId(targetList: list):
         print(str(id) + ": " + targetList[id])
 
 
-def selectFromList(targetList: list, msg: str):
-    printListWithId(targetList)
+def selectFromList(targetList: list, msg: str, defaultSelection: str = '', printList = True):
+    """print and select an item from list, if user input a str matched item in the list, that item id will be return
+
+    Args:
+        targetList (list): the list to select from
+        msg (str): message to show when user is asked for input
+
+    Raises:
+        ValueError: input an number but value is out of range
+
+    Returns:
+        int: select index
+        str: user input str
+    """
+    if printList:
+        printListWithId(targetList)
     while True:
         try:
-            inVal = int(input(msg))
-            return targetList[inVal]
+            defaultId = -1
+            if defaultSelection in targetList:
+                defaultId = targetList.index(defaultSelection)
+            defaultStr = (' (' + str(defaultId) + ')') if defaultId != -1 else ''
+            inVal = input(msg + defaultStr)
+            if inVal.isnumeric():
+                if int(inVal) in range(len(targetList)):
+                    return int(inVal)
+                else:
+                    raise ValueError('out of range')
+            elif inVal in targetList:
+                return targetList.index(inVal)
+            elif inVal == '' and defaultId != -1:
+                return defaultId
+            else:
+                return inVal
+        except Exception as e:
+            print(e)
+
+
+def multiSelectOrAddCategories():
+    while True:
+        try:
+            selectedCategories = []
+            catKeys = categories.keys()
+            inVal = input('select categories(multi select)')
+            lstIn = inVal.split()
+            for val in lstIn:
+                if val.isnumeric():
+                    selectedCategories.append(catKeys[int(val)])
+                else:
+                    selectedCategories.append(val)
+                    if val not in catKeys:
+                        categories[val] = []
+            return selectedCategories
         except Exception as e:
             print(e)
 
@@ -63,13 +130,112 @@ def multiSelectFromList(targetList: list, msg: str):
             print(e)
 
 
+def askAddCategories():
+    catName = input('category name?')
+    if catName == 'q':
+        return
+    else:
+        addCategory(catName)
+
+
+def addCategory(catName:str):
+    """add category to categories
+
+    Args:
+        catName (str): category name
+    """
+    if catName in categories:
+        print(catName, ' existed')
+    else:
+        categories[catName] = []
+
+
+def processSelectorChange(inVal) -> bool:
+    """Check if input is for special process
+
+    Args:
+        inVal (_type_): user input
+
+    Returns:
+        bool: selector has been changed
+    """
+    if type(inVal) is not str:
+        return False
+    lVal = inVal.lower()
+    if lVal == 'q':
+        selectSelector = 'selector'
+        return True
+    elif lVal == 'c':
+        selectSelector = 'category'
+        return True
+    elif lVal == 'f':
+        selectSelector = 'fullList'
+        return True
+    return False
+
+def printCategoriesWithLastWorkoutDelta():
+    id = 0
+    for cat,catWorkouts in categories.items():
+        catDatetime = datetime.datetime.now()
+        for workout in catWorkouts:
+            try:
+                lastWorkoutDate = getClosestWorkoutDateInCsv(workout)
+            except Exception as e:
+                print(e)
+                continue
+            if catDatetime > lastWorkoutDate:
+                catDatetime = lastWorkoutDate
+        catDurationsHrs = (datetime.datetime.now() - catDatetime).total_seconds()/60/60
+        print(id, ': ', cat, '(', int(catDurationsHrs), ')')
+        id += 1
+
+
+def selectCategory() -> int:
+    """select category. if input category name, will ask if add category
+
+    Returns:
+        int: Selected category id
+    """
+    while True:
+        try:
+            printCategoriesWithLastWorkoutDelta()
+            inVal = selectFromList(list(categories.keys()), 'which category?', printList=False)
+            if processSelectorChange(inVal):
+                raise UserCancelException
+            if type(inVal) is int:
+                categories[int(inVal)]
+                return int(inVal)
+            else:
+                if(input('Category ' + inVal + 'does not exist, add it?(N/y)') == 'y'):
+                    addCategory(inVal)
+        except UserCancelException as e: raise
+        except Exception as e:
+            print(e)
+
 def selectWorkoutByCategory():
-    categoriesKeys = list(categories.keys())
-    selectedCat = categories[categoriesKeys[selectFromList(categoriesKeys, 'which category?')]]
-    workout = selectFromList(selectedCat, 'which workout?')
+    cat = selectCategory()
+    selectedCatWorkouts = categories[cat]
+    workout = selectFromList(selectedCatWorkouts, 'which workout?')
     workoutId = workoutNames.index(workout)
     return workoutId, workout
 
+
+def selectSelector():
+    lstSelectors = ['category', 'fullList', 'add']
+    selectFromList(lstSelectors, 'Select workout selector:')
+
+
+def selectWorkout():
+    while True:
+        if workoutSelector == 'category':
+            return selectWorkoutByCategory()
+        elif workoutSelector == 'fullList':
+            return selectWorkoutNumeric()
+        elif workoutSelector == 'add':
+            return askAddWorkout()
+        else:
+            selectSelector()
+    
 
 def selectWorkoutNumeric(inVal):
     workoutId = int(inVal)
@@ -79,6 +245,11 @@ def selectWorkoutNumeric(inVal):
     return workoutId, workout
 
 
+def askAddWorkout():
+    inVal = input('workout name:')
+    return addWorkout(inVal)
+
+
 def addWorkout(inVal):
     if inVal in workoutNames:
         workout = inVal
@@ -86,7 +257,7 @@ def addWorkout(inVal):
     else:
         addConfirm = input('Add workout "' + inVal + '"?(N/y) ')
         if addConfirm.lower() == 'y':
-            workoutCategories = multiSelectFromList(categories.keys(), 'Select category.')
+            workoutCategories = multiSelectOrAddCategories()
 
             global optionJsonData
             optionJsonData['workouts'].append({'Name': inVal, 'categories': workoutCategories})
@@ -109,7 +280,7 @@ def getLastSameWorkoutFromCsv(workout):
                 # skip invalid row
                 continue
             if row[1] == workout:
-                lastTimeStamp = datetime.datetime.strptime(row[0], timeFormatString)
+                lastTimeStamp = getDatetimeFromStr(row[0])
                 lastSameWorkoutHrs = (datetime.datetime.now() - lastTimeStamp).total_seconds()/60/60
                 print("It's " + str(int(lastSameWorkoutHrs)) + ' hrs from last "' + workout + '"')
                 lastRep = row[3]
@@ -161,6 +332,45 @@ def printRecordAndWriteToCsv(timeStamp, workout, weight, rep, lastTimeStamp):
     print('>>>>>>"' + workout + '" ' + str(weight) + '(kg)x' + str(rep) + ', in ' + str(duration) + '@' + timeStamp.strftime(timeFormatString))
 
 
+def selectWorkoutInFullList(lastWorkoutId: int):
+    # print workout options
+    printListWithId(workoutNames) 
+
+    inVal = input("which work out? q:exit, c: select by category, or enter the workout name to search or add (" + str(lastWorkoutId) + ": " + workoutNames[lastWorkoutId] + ') ')
+    # use last workout
+    if inVal == '':
+        workoutId = lastWorkoutId
+        workout = workoutNames[workoutId]
+    # quit
+    elif inVal == 'q':
+        return
+    # select by category 
+    elif inVal == 'c':
+        workoutId, workout = selectWorkoutByCategory()
+    # select from list
+    elif inVal.isnumeric():
+        workoutId, workout = selectWorkoutNumeric(inVal)
+    else:
+        workoutId, workout = addWorkout(inVal)
+    return workoutId, workout
+
+
+def selectWorkout(lastWorkoutId: int, categoryFilterDisplay: str):
+    if categoryFilterDisplay == '':
+        selectWorkoutInFullList(lastWorkoutId)
+    else:
+        selectWorkoutByCategory
+
+
+def checkIfThereIsNoWorkout():
+    while len(workoutNames) == 0:
+        inVal = input("please add first workout:")
+        try:
+            addWorkout(inVal)
+        except Exception as e:
+            print(e)
+
+
 def main():
     readWorkoutOptions()
 
@@ -169,6 +379,7 @@ def main():
     lastWeight = '0'
     lastRep = '20'
     lastTimeStamp = datetime.datetime.now()
+    categoryFilterDisplay = ''
 
     lastWorkoutId = getLastWorkoutIdFromCsv()
 
@@ -176,41 +387,13 @@ def main():
 
     while True:
         # add first workout
-        while len(workoutNames) == 0:
-            inVal = input("please add first workout:")
-            try:
-                addWorkout(inVal)
-            except Exception as e:
-                print(e)
-
-        # print workout options        
-        for id in range(len(workoutNames)):
-            print(str(id) + ": " + workoutNames[id])
-
-        inVal = input("which work out? q:exit, c: select by category, or enter the workout name to search or add (" + str(lastWorkoutId) + ": " + workoutNames[lastWorkoutId] + ') ')
-        # use last workout
-        if inVal == '':
-            workoutId = lastWorkoutId
-            workout = workoutNames[workoutId]
-        # quit
-        elif inVal == 'q':
-            break
-        # select by category 
-        elif inVal == 'c':
-            workoutId, workout = selectWorkoutByCategory()
-        # select from list
-        elif inVal.isnumeric():
-            try:
-                workoutId, workout = selectWorkoutNumeric(inVal)
-            except Exception as e:
-                print(e)
-                continue
-        else:
-            try:
-                workoutId, workout = addWorkout(inVal)
-            except NameError as e:
-                print(e)
-                continue
+        checkIfThereIsNoWorkout()
+        
+        try:
+            workoutId, workout = selectWorkoutInFullList(lastWorkoutId)
+        except Exception as e:
+            print(e)
+            continue
 
         # get last same workout
         lastTimeStamp, lastRep, lastWeight = getLastSameWorkoutFromCsv(workout)
